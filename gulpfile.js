4f2e9f377fc1eca11f6f32c2fb64edd32feddb24
tangqingqing <qq.tang@easycarlife.com>
2016-03-13 18:45:15 +0800

    新建简历

--- gulpfile.js
+++ gulpfile.js
@@ -14,15 +14,17 @@ var gulp = require('gulp'),
 
 //帮助
 gulp.task('help',function(){
-    console.log('	gulp -p	  文件生产环境压缩打包');
+    console.log('	gulp p	  文件生产环境压缩打包');
 
     console.log('	gulp			    文件变动监控打包');
 
+    console.log('	gulp r	          简历版转换sass');
+
     console.log('	gulp help			gulp参数说明');
 
-    console.log('	gulp sass			编译sass');
+    console.log('	gulp sass			sass转换');
 
-    console.log('	gulp sass-min			编译压缩sass');
+    console.log('	gulp sass-min		转换压缩sass');
 
 })
 //生产环境输出
@@ -38,6 +40,12 @@ gulp.task('watch-sass',function(){
     gulp.watch(['./assets/sass/*.scss','./assets/sass/mixin/*.scss'],['sass']);
 })
 
+gulp.task('r',function(){
+    gulp.src('./resume_assets/css/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(concat('tqqStyle.css'))
+        .pipe(gulp.dest('./resume_assets/css'))
+})
 gulp.task('min',function(){
     gulp.src([
         'bower_components/jquery/dist/jquery.min.js',
@@ -52,8 +60,9 @@ gulp.task('min',function(){
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/css'))
         .pipe(concat('production.min.css'))
-        .pipe(gulp.dest('./dist'))
         .pipe(minifycss())
+        .pipe(gulp.dest('./dist'))
+
 })
 //sass编译，普通版（未压缩）
 gulp.task('sass',function(){
@@ -68,11 +77,14 @@ gulp.task('sass',function(){
 //sass编译，压缩版
 gulp.task('sass-min',function(){
     gulp.src('./assets/sass/*.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/css'))
         .pipe(concat('tqqStyle.min.css'))
-        .pipe(gulp.dest('./dist'))
         .pipe(minifycss())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist'))
+
 })
 //angular 编译-合并
 gulp.task('angular',function(){

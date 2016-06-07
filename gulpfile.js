a69ee7ebefb1336e18a3254857441b388327ca5b
tangqingqing <827423258@qq.com>
2016-06-07 09:51:10 +0800

    ss

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,41 @@ var gulp = require('gulp'),
     ngAnnotate = require('gulp-ng-annotate'),
     ngMin = require('gulp-ngmin'),
     uglify = require('gulp-uglify'),
+    clean = require('gulp-clean'),
     sass = require('gulp-sass'),
     minifycss = require('gulp-minify-css'),
     sourcemaps = require('gulp-sourcemaps'),
+    livereload = require('gulp-livereload'),
+    open = require('gulp-open'),
     stripDebug = require('gulp-strip-debug');
 
+const config={
+    JS_WATCH:['./assets/js/*.js','./assets/js/*/*.js','./module/*/*.js'],
+    SASS_WATCH:['./assets/sass/*.scss','./module/*/*.scss','./module/*.scss'],
+    JS_PATH:[
+        'bower_components/jquery/dist/jquery.js',
+        'bower_components/moment/moment.js',
+        'bower_components/moment/locale/zh-cn.js',
+        'bower_components/angular/angular.js',
+        'bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+        'bower_components/angular-ui-router/release/angular-ui-router.js',
+        'bower_components/tqqUi-angular-pagination/dist/tqqUi.js',
+        'bower_components/angular-animate/angular-animate.js',
+        'bower_components/angular-toastr/dist/angular-toastr.tpls.js',
+        'bower_components/jquery-form/jquery.form.js',
+        'bower_components/angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.js',
+        'bower_components/bootstrap-daterangepicker/daterangepicker.js',
+        'bower_components/alertifyjs/dist/js/ngAlertify.js',
+    ],
+    CSS_PATH:[
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        'bower_components/tqqUi-angular-pagination/dist/tqqUi.css',
+        'bower_components/angular-toastr/dist/angular-toastr.css',
+        'bower_components/angular-bootstrap-lightbox/dist/angular-bootstrap-lightbox.css',
+        'bower_components/bootstrap-daterangepicker/daterangepicker.css',
+        'bower_components/alertifyjs/dist/css/alertify.css',
+    ]
+}
 
 //帮助
 gulp.task('help',function(){
@@ -26,38 +56,46 @@ gulp.task('help',function(){
 
 })
 //生产环境输出
-gulp.task('p',['sass-min','angular-min','min']);
+gulp.task('p',['sass-min','angular-min','min','move']);
 
 gulp.task('default',['watch-angular','watch-sass']);
 //监视编译angular
 gulp.task('watch-angular',function(){
-     gulp.watch(['./assets/js/*.js','./assets/js/*/*.js','./module/*/*.js'],['angular'])
+    gulp.watch(config.JS_WATCH,['angular'])
 })
 //监视sass
 gulp.task('watch-sass',function(){
-    gulp.watch(['./assets/sass/*.scss','./assets/sass/mixin/*.scss','./module/*/*.scss'],['sass']);
+    gulp.watch(config.SASS_WATCH,['sass']);
 })
 
 gulp.task('min',function(){
-    gulp.src([
-        'bower_components/jquery/dist/jquery.min.js',
-        'bower_components/angular/angular.js',
-        'bower_components/angular-ui-router/release/angular-ui-router.js',
-        'bower_components/tqqUi-angular-pagination/dist/tqqUi.js',
-    ])
+    gulp.src(config.JS_PATH)
         .pipe(uglify({outSourceMap:false}))
-        .pipe(concat('angular.min.js'))
+        .pipe(stripDebug())  //console
+        .pipe(uglify({outSourceMap:false}))
+        .pipe(concat('production.min.js'))
         .pipe(gulp.dest('./dist'));
-    gulp.src(['bower_components/bootstrap/dist/css/bootstrap.css','bower_components/tqqUi-angular-pagination/dist/tqqUi.css'])
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./src/css'))
+    gulp.src(config.CSS_PATH)
+        .pipe(sourcemaps.init())
         .pipe(concat('production.min.css'))
-        .pipe(gulp.dest('./dist'))
         .pipe(minifycss())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dist'))
+
+})
+
+//移动文件
+gulp.task('move',function(){
+    gulp.src('./assets/img/*').pipe(gulp.dest('./dist/img'));
+    gulp.src('./assets/ogg/*').pipe(gulp.dest('./dist/ogg'))
+})
+//删除src
+gulp.task('clean',function(){
+    gulp.src('./src').pipe(clean());
 })
 //sass编译，普通版（未压缩）
 gulp.task('sass',function(){
-    gulp.src(['./assets/sass/*.scss','./module/*/*.scss'])
+    gulp.src(config.SASS_WATCH)
         .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/css'))
@@ -67,7 +105,7 @@ gulp.task('sass',function(){
 })
 //sass编译，压缩版
 gulp.task('sass-min',function(){
-    gulp.src(['./assets/sass/*.scss','./module/*/*.scss','./module/*.scss'])
+    gulp.src(config.SASS_WATCH)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./src/css'))
         .pipe(concat('tqqStyle.min.css'))
@@ -76,14 +114,14 @@ gulp.task('sass-min',function(){
 })
 //angular 编译-合并
 gulp.task('angular',function(){
-    gulp.src(['./assets/js/*.js','./assets/js/*/*.js','./module/*/*.js'])
+    gulp.src(config.JS_WATCH)
         .pipe(ngAnnotate())
         .pipe(concat('my.js'))
         .pipe(gulp.dest('./dist'))
 })
 //angular 编译合并压缩
 gulp.task('angular-min',function(){
-    gulp.src(['./assets/js/*.js','./assets/js/*/*.js','./module/*/*.js'])
+    gulp.src(config.JS_WATCH)
         .pipe(ngAnnotate())
         .pipe(ngMin({dynamic:false}))
         .pipe(stripDebug())  //console
